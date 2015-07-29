8ba3d4c935704998e8a75d97a11e019b5e3a5065
twinsdbv <twinsdbv@gmail.com>
2015-07-29 19:12:42 +0300

    added: async for working with mongoose and express;

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
     , router = express.Router()
+    , async = require('async')
     , models = require('../models');
 
 router.use('/room', require('./room'));
@@ -7,10 +8,22 @@ router.use('/chat', require('./chat'));
 router.use('/users', require('./users'));
 
 router.get('/', function(req, res) {
-
     var Room = models.Room.model;
-    Room.find({}, function (err, rooms) {
-        res.render('index', {rooms: rooms});
+
+    async.parallel({
+            // 1st parallel function
+            getRooms: function (callback) {
+                Room.find({}).exec(callback);
+            },
+
+            // 2nd parallel function
+            getOneRoom: function (callback) {
+                Room.findOne({'name': '210708'}).exec(callback);
+            }
+        }
+        // process results
+    , function (err, result) {
+        res.render('index', {rooms: result.getRooms, oneRoom: result.getOneRoom});
     });
 });
 
