1bec360a507357c8ac160e9a960c74508080a419
sergey.illarionov <easthunch@gmail.com>
2015-08-18 10:49:50 +0300

    add: skype emoticon pack

--- modules/mmReceiver/smiley-pack/pack.js
+++ modules/mmReceiver/smiley-pack/pack.js
@@ -1,39 +1,51 @@
 var SmileyPack = (function () {
     var settings = {
             path: {
-                toPack: 'pidgin-smiley-pack',
-                toImg: 'pidgin-smiley-pack/img',
+                toPack: './smiley-pack/skype-smiley-pack/',
                 toData: './smiley-pack/data.json'
-            }
+            },
+            imgExtension: '.gif'
         },
         smileyData = {},
+        smileyExpand = {},
+
+
 
         init = function () {
-
             Get.dataJSON();
         },
 
-        getSmileyData = function () {
-            return smileyData;
+        getImage = function (emoticon) {
+            console.log(smileyExpand);
+            console.log(smileyData);
+            var emoticonName = (smileyExpand[emoticon]) ? (smileyExpand[emoticon]) : emoticon;
+            return (smileyData[emoticonName]) ? Get.image(emoticonName) :  emoticon;
         },
 
         Get = {
 
             dataJSON: function () {
                 $.get(settings.path.toData, function (data) {
-                    data = data[0];
-                    for (var key in data) {
-                        for(var i=0; i < data[key].length; i++) {
-                            smileyData[data[key][i]] = key;
+                    smileyData = data[0];
+                    for (var key in smileyData) {
+                        for(var i=0; i < smileyData[key].length; i++) {
+                            smileyExpand[smileyData[key][i]] = key;
                         }
                     }
                 });
 
+            },
+
+            image: function (emoticonName) {
+                var mainSmileyInSeries = smileyData[emoticonName][0],
+                    fileName = emoticonName.replace(/:/g, '');
+
+                return '<img alt="' + mainSmileyInSeries + '" src="' + settings.path.toPack + fileName + settings.imgExtension + '" />'
             }
         };
 
     return {
         init: init,
-        getSmileyData: getSmileyData
+        getImage: getImage
     }
 })();
