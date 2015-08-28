0fad450d1816f56792b43132344bef19b2cdfa68
sergey.illarionov <easthunch@gmail.com>
2015-08-28 15:31:12 +0300

    add: chat.js

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,8 +1,6 @@
-var express = require('express')
-  , router = express.Router();
 
-router.get('/:id', function (req, res) {
-  res.render('chat/index');
-});
-
-module.exports = router;
+module.exports = {
+    index: function (req, res) {
+        res.render('chat/index');
+    }
+};
