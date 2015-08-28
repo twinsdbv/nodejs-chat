d5a86ca0776ce41ebb371930520fb7c8d21b3e5d
sergey.illarionov <easthunch@gmail.com>
2015-08-28 15:31:38 +0300

    add: controller room.js

--- controllers/room.js
+++ controllers/room.js
@@ -1,18 +1,16 @@
-var express = require('express')
-  , router = express.Router()
-  , models = require('../models');
+var models = require('../models');
 
-router.post('/create', function (req, res) {
-    var name = req.body.name;
-    var description = req.body.description;
+module.exports = {
+    index: function (req, res) {
+        var name = req.body.name;
+        var description = req.body.description;
 
-    var RoomObj = new models.Room.model({
-        'name': name,
-        'description': description
-    });
-    RoomObj.save();
+        var RoomObj = new models.Room.model({
+            'name': name,
+            'description': description
+        });
+        RoomObj.save();
 
-    res.redirect('/chat/' + RoomObj.id);
-});
-
-module.exports = router;
+        res.redirect('/chat/' + RoomObj.id);
+    }
+};
