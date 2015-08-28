8162d197b62354320d04a09496cd2b7ce7f5f0f4
sergey.illarionov <easthunch@gmail.com>
2015-08-28 15:31:46 +0300

    add: controller user.js

---
+++ controllers/user.js
@@ -0,0 +1,17 @@
+
+
+
+module.exports = {
+  index: function (req, res) {
+
+  },
+
+  check: function (req, res) {
+    var email = req.body.email;
+    var sess = req.session;
+
+    sess.email = email;
+
+    res.redirect('/');
+  }
+};
