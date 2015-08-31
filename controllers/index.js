239baf6f9e2da489a53505ad813ef95ae6a929bc
sergey.illarionov <easthunch@gmail.com>
2015-08-31 15:19:59 +0300

    add: condition with session

--- controllers/index.js
+++ controllers/index.js
@@ -8,32 +8,30 @@ var user = require('./user');
 module.exports = function (app, io) {
     app.get('/', function(req, res) {
         var sess = req.session;
-
-        if(sess.email) {
-            var Room = models.Room.model;
-
-            async.parallel({
-                    // 1st parallel function
-                    getRooms: function (callback) {
-                        Room.find({}).exec(callback);
-                    },
-
-                    // 2nd parallel function
-                    getOneRoom: function (callback) {
-                        Room.findOne({'name': '210708'}).exec(callback);
-                    }
-                }
-                // process results
-                , function (err, result) {
-                    res.render('index', {rooms: result.getRooms, oneRoom: result.getOneRoom});
-                });
-
-            res.cookie('email' , sess.email);
-            require('../helpers/chat-sockets')(app, io);
-        }
-        else{
+        if(!sess.email) {
             res.redirect('/login');
+            return '';
         }
+
+        var Room = models.Room.model;
+
+        async.parallel({
+                // 1st parallel function
+                getRooms: function (callback) {
+                    Room.find({}).exec(callback);
+                },
+
+                // 2nd parallel function
+                getOneRoom: function (callback) {
+                    Room.findOne({'name': '210708'}).exec(callback);
+                }
+            }
+            // process results
+            , function (err, result) {
+                res.render('index', {rooms: result.getRooms, oneRoom: result.getOneRoom});
+            });
+
+        res.cookie('email' , sess.email);
     });
 
     app.get('/chat/:id', chat.index);
